843dd38b89443f567a426ba0ee1e068d19c263e7
Avi Ihej <aviihej@gmail.com>
2018-09-30 16:57:58 -0400

    link actions with reducer

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
-
 //This is our binding to the store
 // It is also a higher order component
 import { connect } from 'react-redux'; 
+import {  } from 'testActions'
 
-// Recieved the state from the store
+// Recieved the state data from the store
 // Test matches the name of the reducer that we used in our root reducer
 //So we can access the data for test just by using state.test. The date
 const mapState = (state) => ({
     data: state.test.data
 })
+
+// now we want to dispatch actions to the store
+caonst actions = {
+
+}
 
 class TestComponent extends Component{
     render(){
--- src/features/testarea/testReducer.js
+++ src/features/testarea/testReducer.js
@@ -1,10 +1,19 @@
+import { INCREMENT_COUNTER, DECREMENT_COUNTER } from "./testConstants";
+
 const initialState = {
     data: 433564
 }
 
 //Takes a state and action
 const testReducer = (state = initialState, action) => {
-    return state
+    switch (action.type) { //ACTION IS BASED ON WHITCH TYPE OF ACTION IS COMING IN. CONDITION IS THE ACTION TYPE
+        case INCREMENT_COUNTER // IN THE CASE OF INCREMENT COUNTER
+            return {...state, data: state.data +1 };
+            case DECREMENT_COUNTER:
+            return{...state, data: state.data -1 };
+            default:
+            return state;
+    }
 }
 
 export default testReducer
