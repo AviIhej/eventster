3676612a0b8951631dccc8a7a6361b64174879a5
Avi Ihej <aviihej@gmail.com>
2018-09-30 16:27:19 -0400

    connect store to our application: map state from store to our component

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -3,7 +3,6 @@ import testReducer from './testReducer';
 
 const rootReducer = combineReducers({
     test: testReducer
-
 })
 
 export default rootReducer;
--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,13 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+//This is our binding to the store
+// It is also a higher order component
+import { connect } from 'react-redux'; 
+
+// Recieved the state from the store
+// Test matches the name of the reducer that we used in our root reducer
+//So we can access the data for test just by using state.test. The date
+const mapState = (state) => ({
+    data: state.test.data
+})
 
 class TestComponent extends Component{
     render(){
         return(
             <div>
                 <h1>Test Area</h1>
+                <h3>The answer is: {this.props.data}</h3>
             </div>
         )
     }
 }
-
-export default TestComponent;
+//We need to map the state from the store to our component
+// Now our state is being mapped to our component
+export default connect(mapState)(TestComponent)
