53ead057b5787f640b06260675ed2d3a5787d3eb
Avi Ihej <aviihej@gmail.com>
2018-10-01 01:07:47 -0400

    create actions for events

---
+++ src/features/event/eventActions.jsx
@@ -0,0 +1,44 @@
+import {    CREATE_EVENT, UPDATE_EVENT, DELETE_EVENT } from './eventConstants';
+
+export const createEvent = (event) => {
+    return {
+        type: CREATE_EVENT,
+        payload:{
+            event
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export const updateEvent = (event) => {
+    return{
+        type: DELETE_EVENT,
+        payload: {
+            event
+        }
+    }
+}
+
+export const deleteEvent = (eventID) => {
+    return{
+        type: DELETE_EVENT,
+        payload: {
+            eventID 
+        }
+    }
+}
