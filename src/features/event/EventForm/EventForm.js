ec8430921e88f48b0c2e724a63b0ef2c80c75088
Avi Ihej <aviihej@gmail.com>
2018-10-06 18:12:47 -0400

    fix form submission

--- src/features/event/EventForm/EventForm.js
+++ src/features/event/EventForm/EventForm.js
@@ -13,20 +13,14 @@ import SelectInput from '../../../app/common/form/SelectInput';
 const mapState = (state, ownProps) => {
   const eventId = ownProps.match.params.id;
 
-  let event = {
-    title: '',
-    date: '',
-    city: '',
-    venue: '',
-    hostedBy: '', 
-  }
+  let event = {};
 
   if(eventId && state.events.length > 0) {
     event = state.events.filter(event => event.id === eventId)[0]
   }
 
   return{
-    event
+    initialValues: event 
   }
 }
 
@@ -46,16 +40,16 @@ const category = [
 
 class EventForm extends Component {
 
-  onFormSubmit = (event) => {
-    event.preventDefault();
-    if(this.state.event.id){
-      this.props.updateEvent(this.state.event)
+  onFormSubmit = (values) => {
+    if(this.props.initialValues.id){
+      this.props.updateEvent(values)
       this.props.history.goBack(); //Takes user back to the event detailed
     }else{
       const newEvent = {
-        ...this.state.event,
+        ...values,
         id: cuid(),
-        hostPhotoURL: '/assets/user.png'
+        hostPhotoURL: '/assets/user.png',
+        hostedBy: 'Bob' 
       }
       this.props.createEvent(newEvent)
       this.props.history.push('/events')
@@ -70,7 +64,7 @@ class EventForm extends Component {
           <Grid.Column width={10}>
           <Segment>
             <Header sub color='teal' content='Event Details'/>
-            <Form onSubmit={this.onFormSubmit}>
+            <Form onSubmit={this.props.handleSubmit(this.onFormSubmit)}>
               <Field name='title' type='text' component={TextInput} placeholder='Give your event a name' />
               <Field name='category' type='text' options={category} component={SelectInput} placeholder='What is your event about' />
               <Field name='description' type='text' rows={3} component={TextArea} placeholder='Tell us about your event' />
@@ -91,4 +85,4 @@ class EventForm extends Component {
   }
 }
 
-export default connect(mapState, actions)(reduxForm({form: 'eventForm'})(EventForm));
+export default connect(mapState, actions)(reduxForm({form: 'eventForm', enableReinitialize: true})(EventForm));
