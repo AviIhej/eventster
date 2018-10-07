1e0116552663ad8996f794b8b4ecf2cfb5230063
Avi Ihej <aviihej@gmail.com>
2018-10-07 03:07:56 -0400

    fix date picker issue

--- src/features/event/EventForm/EventForm.js
+++ src/features/event/EventForm/EventForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { reduxForm, Field } from 'redux-form';
+import moment from 'moment';
 import { composeValidators, combineValidators, isRequired, hasLengthGreaterThan } from 'revalidate'
 import cuid from 'cuid';
 import { Segment, Form, Button, Grid, Header  } from 'semantic-ui-react';
@@ -48,12 +49,14 @@ const validate = combineValidators({
     hasLengthGreaterThan(4)({message: 'Description needs to be at least 5 characters'})
   ) (),
   city: isRequired('city'),
-  venue: isRequired('venue')
+  venue: isRequired('venue'),
+  date: isRequired({message: 'date'})
 })
 
 class EventForm extends Component {
 
   onFormSubmit = (values) => {
+    values.date = moment(values.date).format();
     if(this.props.initialValues.id){
       this.props.updateEvent(values)
       this.props.history.goBack(); //Takes user back to the event detailed
@@ -89,7 +92,7 @@ class EventForm extends Component {
                name='date'
                type='text' 
                component={DateInput} 
-               dateFormat='YYYY/MM/DD HH:mm' 
+               dateFormat='YYYY-MM-DD HH:mm' 
                timeFormat='HH:mm'
                showTimeSelect
                placeholder='Date and time of event' 
