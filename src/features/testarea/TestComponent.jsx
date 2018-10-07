36e393e184f41475f76e84b714cd50ec669ca575
Avi Ihej <aviihej@gmail.com>
2018-10-07 15:01:22 -0400

    set up google maps

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 //This is our binding to the store
 // It is also a higher order component
-import { connect } from 'react-redux'; 
-import { Button } from 'semantic-ui-react'
+import { connect } from 'react-redux';
+import GoogleMapReact from 'google-map-react';
+import { Button, Icon } from 'semantic-ui-react'
 import Script from 'react-load-script';
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 
@@ -11,6 +12,7 @@ import { incrementCounter, decrementCounter } from './testActions'
 // Recieved the state data from the store
 // Test matches the name of the reducer that we used in our root reducer
 //So we can access the data for test just by using state.test. The date
+
 const mapState = (state) => ({
     data: state.test.data,
 })
@@ -21,7 +23,17 @@ const actions = {
     decrementCounter
 }
 
+const Marker = () => <Icon name='marker' size='big' color='red' /> 
+
 class TestComponent extends Component{
+
+    static defaultProps = {
+        center: {
+          lat: 59.95,
+          lng: 30.33
+        },
+        zoom: 11
+    };
 
     state= {
         address: '',
@@ -52,10 +64,10 @@ class TestComponent extends Component{
         
         return(
             <div>
-                <Script 
+                {/* <Script 
                     url='https://maps.googleapis.com/maps/api/js?key=AIzaSyBxhsMsEZYwqUG1V7NITAAk-F1dL4cU3LU&libraries=places' 
                     onLoad={this.handleScriptLoad}
-                />
+                /> */}
                 <h1>Test Area</h1>
                 <h3>The answer is: { data}</h3>
                 <Button onClick={incrementCounter} color='green' content='Increment' />
@@ -65,6 +77,20 @@ class TestComponent extends Component{
                 {this.state.scriptLoaded &&  <PlacesAutocomplete inputProps={inputProps} />}
                     <button type="submit">Submit</button>
                 </form>
+
+                      <div style={{ height: '300px', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyBxhsMsEZYwqUG1V7NITAAk-F1dL4cU3LU' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <Marker
+            lat={59.955413}
+            lng={30.337844}
+            text={'Kreyser Avrora'}
+          />
+        </GoogleMapReact>
+      </div>
             </div>
         )
     }
